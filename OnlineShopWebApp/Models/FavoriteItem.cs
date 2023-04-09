11fc23e85851fd1a1ceb02b2464974fdee53d0ec
Павел Грабовский <araa9977@mail.ru>
2023-04-10 05:57:40 +0700

    Добавил избранное

---
+++ OnlineShopWebApp/Models/FavoriteItem.cs
@@ -0,0 +1,8 @@
+﻿namespace OnlineShopWebApp.Models
+{
+    public class FavoriteItem
+    {
+        public Guid Id { get; set; }
+        public Product Product { get; set; }
+    }
+}
---
+++ OnlineShopWebApp/Models/Favorites.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineShopWebApp.Models
+{
+    public class Favorites
+    {
+        public Guid Id { get; set; }
+        public string UserId { get; set; }
+        public List<FavoriteItem> Items { get; set; }
+    }
+}
