84401fa7a8a2fd88da56c83362cb8ef44e4095b4
Павел Грабовский <araa9977@mail.ru>
2023-04-24 21:05:20 +0700

    Добавлена авторизация пользователя

---
+++ OnlineShopWebApp/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopWebApp.Models;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(Login login)
+        {
+            return View();
+        }
+    }
+}
