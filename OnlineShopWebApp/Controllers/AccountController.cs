5a1da5987032e263853962cd786caf0afff1f220
Павел Грабовский <araa9977@mail.ru>
2023-04-24 22:25:32 +0700

    Добавлена регистрация аккаунта

--- OnlineShopWebApp/Controllers/AccountController.cs
+++ OnlineShopWebApp/Controllers/AccountController.cs
@@ -9,9 +9,25 @@ namespace OnlineShopWebApp.Controllers
         {
             return View();
         }
+
         [HttpPost]
         public IActionResult Login(Login login)
         {
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(Register register)
+        {
+            if(register.Password == register.ConfirmPassword)
+            {
+                return RedirectToAction("Index", "Home");
+            }
             return View();
         }
     }
---
+++ OnlineShopWebApp/Models/Register.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopWebApp.Models
+{
+    public class Register
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
