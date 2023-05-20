2e4fad70be707685bc1c7ee2c6debff4abd42958
Павел Грабовский <araa9977@mail.ru>
2023-05-20 22:31:53 +0700

    Добавленно логирование

--- OnlineShopWebApp/Program.cs
+++ OnlineShopWebApp/Program.cs
@@ -1,15 +1,17 @@
 using OnlineShopWebApp;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
+
+// Add services to the container.
 builder.Services.AddSingleton<IFavoritesRepository, FavoritesInMemoryRepository>();
 builder.Services.AddSingleton<IProductsRepository, ProductsInMemoryRepository>();
 builder.Services.AddSingleton<ICartsRepository, CartsInMemoryRepository>();
 builder.Services.AddSingleton<IOrdersRepository, OrdersInMemoryRepository>();
 builder.Services.AddSingleton<IRolesRepository, RolesInMemoryRepository>();
 
-
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -21,6 +23,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseSerilogRequestLogging();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
