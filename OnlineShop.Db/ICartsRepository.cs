8853e47d7f4a2e5457ad951cd77676c95f993e95
Павел Грабовский <araa9977@mail.ru>
2023-09-09 09:51:38 +0700

    Добавлена корзина в проект БД

---
+++ OnlineShop.Db/ICartsRepository.cs
@@ -0,0 +1,13 @@
+﻿using OnlineShop.Db.Models;
+
+namespace OnlineShop.Db
+{
+    public interface ICartsRepository
+    {
+        void Add(string userId, Product product);
+        Cart TryGetByUserId(string userId);
+        void DecreaseAmount(string userId, Product product);
+        void Remove(string userId, Product product);
+        void Clear(string userId);
+    }
+}
---
+++ OnlineShop.Db/Models/Cart.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineShop.Db.Models
+{
+    public class Cart
+    {
+        public Guid Id { get; set; }
+        public string UserId { get; set; }
+        public ICollection<CartItem> Items { get; set; } = new List<CartItem>();
+    }
+}
