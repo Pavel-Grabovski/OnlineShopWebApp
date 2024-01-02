3ae33142067b5a2a91db74d269a73f4483370d11
Павел Грабовский <araa9977@mail.ru>
2024-01-03 02:13:08 +0700

    Add метод удаление по Id продукта

--- OnlineShop.BL/Interfaces/IProductsServicies.cs
+++ OnlineShop.BL/Interfaces/IProductsServicies.cs
@@ -7,6 +7,7 @@ public interface IProductsServicies
 	Task<IEnumerable<Product>> GetAllAsync();
 	Task<Product> TryGetByIdAsync(Guid id);
 	Task DeleteAsync(Product product);
-	Task AddAsync(Product product);
+	Task DeleteAsync(Guid id);
+    Task AddAsync(Product product);
 	Task UpdateAsync(Product product);
 }
