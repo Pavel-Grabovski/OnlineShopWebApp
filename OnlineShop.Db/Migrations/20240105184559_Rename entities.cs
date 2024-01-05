0376afe0e7cb66123be7b61fb036e4347a9837d0
Павел Грабовский <araa9977@mail.ru>
2024-01-06 02:45:06 +0700

    миграция

---
+++ OnlineShop.Db/Migrations/20240105184559_Rename entities.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineShop.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class Renameentities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartItems_Carts_CartId",
+                table: "CartItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartItems_Orders_OrderId",
+                table: "CartItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_UserDeliveryInfo_UserInfoId",
+                table: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "UserDeliveryInfo");
+
+            migrationBuilder.RenameColumn(
+                name: "OrderId",
+                table: "CartItems",
+                newName: "OrderEntityId");
+
+            migrationBuilder.RenameColumn(
+                name: "CartId",
+                table: "CartItems",
+                newName: "CartEntityId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CartItems_OrderId",
+                table: "CartItems",
+                newName: "IX_CartItems_OrderEntityId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CartItems_CartId",
+                table: "CartItems",
+                newName: "IX_CartItems_CartEntityId");
+
+            migrationBuilder.CreateTable(
+                name: "UserDeliveryInfoEntity",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Patronymic = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserDeliveryInfoEntity", x => x.Id);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartItems_Carts_CartEntityId",
+                table: "CartItems",
+                column: "CartEntityId",
+                principalTable: "Carts",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartItems_Orders_OrderEntityId",
+                table: "CartItems",
+                column: "OrderEntityId",
+                principalTable: "Orders",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_UserDeliveryInfoEntity_UserInfoId",
+                table: "Orders",
+                column: "UserInfoId",
+                principalTable: "UserDeliveryInfoEntity",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartItems_Carts_CartEntityId",
+                table: "CartItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartItems_Orders_OrderEntityId",
+                table: "CartItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_UserDeliveryInfoEntity_UserInfoId",
+                table: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "UserDeliveryInfoEntity");
+
+            migrationBuilder.RenameColumn(
+                name: "OrderEntityId",
+                table: "CartItems",
+                newName: "OrderId");
+
+            migrationBuilder.RenameColumn(
+                name: "CartEntityId",
+                table: "CartItems",
+                newName: "CartId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CartItems_OrderEntityId",
+                table: "CartItems",
+                newName: "IX_CartItems_OrderId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CartItems_CartEntityId",
+                table: "CartItems",
+                newName: "IX_CartItems_CartId");
+
+            migrationBuilder.CreateTable(
+                name: "UserDeliveryInfo",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Patronymic = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserDeliveryInfo", x => x.Id);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartItems_Carts_CartId",
+                table: "CartItems",
+                column: "CartId",
+                principalTable: "Carts",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartItems_Orders_OrderId",
+                table: "CartItems",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_UserDeliveryInfo_UserInfoId",
+                table: "Orders",
+                column: "UserInfoId",
+                principalTable: "UserDeliveryInfo",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
